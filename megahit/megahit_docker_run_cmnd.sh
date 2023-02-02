e8ff12f7a5b21ac1c694b11359b31810a87a597f
muhit-emon <82660145+muhit-emon@users.noreply.github.com>
2023-02-02 10:07:30 -0500

    Update megahit_docker_run_cmnd.sh

--- megahit/megahit_docker_run_cmnd.sh
+++ megahit/megahit_docker_run_cmnd.sh
@@ -4,7 +4,7 @@ f1=$(basename $1) # $1 or the first argument of this bash script should be the h
 f2=$(basename $2) # $2 or the second argument of this bash script should be the host file system absolute path of rev file. This rev file could be quality controlled (optional).
 # $3 or the third argument of this bash script should be the absolute path of the directory on host machine where the output files will be generated.
 # $4 or the fourth argument of this bash script is the number of cpus to be utilized.
-# $5 or the fifth argument of this bash script is the number of cpus to be utilized.
+# $5 or the fifth argument of this bash script is output file prefix.
 
 sudo docker run -v $1:/home/Assembly_megahit/input/$f1 -v $2:/home/Assembly_megahit/input/$f2 -v $3:/home/Assembly_megahit/output megahit ./MEGAHIT-1.2.9-Linux-x86_64-static/bin/megahit --presets meta-sensitive -1 ./input/$f1 -2 ./input/$f2 -t $4 -f -o ./output --out-prefix $5
 
